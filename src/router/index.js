a4d6165dd3569351bff431d85b08b056fe0bc524
lu-shuaigege <1769059514@qq.com>
2019-07-23 14:27:20 +0800

    img name

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/views/home/home.vue'
+import about from '@/views/about/about.vue'
 
 Vue.use(Router)
-
+export const contentsRouter = [{
+  path: '/',
+  redirect: 'home',
+},
+{
+  path: '/home',
+  name: 'home',
+  meta: {
+    title: '主页'
+  },
+  component: () => import('../views/home/home.vue')
+},
+{
+  path: '/about',
+  name: 'about',
+  meta: {
+    title: '关于我们'
+  },
+  component: () => import('../views/about/about.vue')
+},
+]
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  // routes,
+  routes: contentsRouter,
 })
